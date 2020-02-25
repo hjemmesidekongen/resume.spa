d2e170543a901c41aa1496b9de8ee6336452bab8
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-25 12:01:04 +0100

    Use store to show company data and sort them

--- src/companies/Companies.js
+++ src/companies/Companies.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import store from '../store/companies.json';
 import CompaniesList from './CompaniesList';
 
-const Companies = () => (
-  <>
-    <CompaniesList />
-  </>
-);
+const Companies = () => {
+  const companies = store.data;
+  const orderedCompanies = companies.sort((a, b) => {
+    if (a.hiredAt > b.hiredAt) return 1;
+    if (b.hiredAt > a.hiredAt) return -1;
+
+    return 0;
+  });
+
+  return (
+    <>
+      <CompaniesList companies={orderedCompanies} />
+    </>
+  );
+};
 
 export default Companies;
