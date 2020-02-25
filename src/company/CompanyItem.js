a86327a2c1e97e34e34cae9d48a317df2f9ae283
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-25 09:54:53 +0100

    Add CompanyItem (tdd)

---
+++ src/company/CompanyItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CompanyItem = ({name, logo, url, city, hiredAt, endedAt}) => {
+    return (
+        <div className="company-item">
+            <span data-testid="name">
+                {name}
+            </span>
+            <span data-testid="logo">
+                {logo}
+            </span>
+            <span data-testid="url">
+                {url}
+            </span>
+            <span data-testid="city">
+                {city}
+            </span>
+            <span data-testid="hiredAt">
+                {hiredAt}
+            </span>
+            <span data-testid="endedAt">
+                {endedAt}
+            </span>
+        </div>
+    )
+};
+
+CompanyItem.defaultProps = {
+    logo: '/example-logo.svg',
+};
+
+CompanyItem.propTypes = {
+    name: PropTypes.string.isRequired,
+    logo: PropTypes.string,
+    url: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    hiredAt: PropTypes.number.isRequired,
+    endedAt: PropTypes.number.isRequired,
+};
+
+export default CompanyItem;
