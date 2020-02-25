1d38ddc44db16636a3819a8f29f28db8a219226f
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-25 09:54:42 +0100

    Add companies list (tdd)

---
+++ src/company/CompaniesList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CompaniesList = ({ companies }) => {
+    if (companies.length === 0) {
+        return (
+            <div data-testid="emptyMessage">
+                Ingen virksomheder tilføjet.
+            </div>
+        );
+    }
+
+    return (
+        <div data-testid="list">
+            {companies.map(company => (
+                <div data-testid="company" key={`${company.name}-${company.hiredAt}`}>
+                    {company.name}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+CompaniesList.defaultProps = {
+    companies: [],
+};
+
+CompaniesList.propTypes = {
+    companies: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            logo: PropTypes.string.isRequired,
+            url: PropTypes.string.isRequired,
+            city: PropTypes.string.isRequired,
+            hiredAt: PropTypes.number.isRequired,
+            endedAt: PropTypes.number.isRequired,
+        }),
+    ),
+};
+
+export default CompaniesList;
